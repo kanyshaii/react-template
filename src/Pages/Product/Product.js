a98d02812c32d51ea84b48dc055c94cdd51abb67
kanyshaii <kanyshaibaktybekova07@gmail.com>
2024-06-11 14:16:56 +0600

    added new style for product

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,3 +1,34 @@
+// import { useContext } from "react";
+// import { useMatch } from "react-router-dom"
+// import { AppContext } from "../../App";
+// import NotFound from "../NotFound/NotFound";
+// import { AddToCart } from "../../Components/AddToCart/AddToCart";
+
+// export default function Product() {
+//   const { params } = useMatch("/products/:slug");
+//   const { products } = useContext(AppContext);
+
+//   const product = products.find(product => product.slug === params.slug);
+
+//   if (!product) {
+//     return <NotFound />
+//   }
+
+//   return (
+//     <div className="Product">
+//       <div>
+//         <img src={product.img} alt={product.name} />
+//       </div>
+//       <div>
+//         <h1>{product.name}</h1>
+//         <p><strong>Price:</strong> {product.price}$</p>
+//         <p><strong>Description: </strong>{product.desc}</p>
+//         <AddToCart product={product} />
+//       </div>
+//     </div>
+//   )
+// }
+import './Product.css';
 import { useContext } from "react";
 import { useMatch } from "react-router-dom"
 import { AppContext } from "../../App";
@@ -17,12 +48,12 @@ export default function Product() {
   return (
     <div className="Product">
       <div>
-        <img src={product.img} alt={product.name} />
+        <img className='product_img' src={product.img} alt={product.name} />
       </div>
-      <div>
-        <h1>{product.name}</h1>
-        <p><strong>Price:</strong> {product.price}$</p>
-        <p><strong>Description: </strong>{product.desc}</p>
+      <div className='product_right'>
+        <h1 className="product_tit">{product.name}</h1>
+        <p className='product_price_all'><strong className="product_price">Price:</strong> {product.price}$</p>
+        <p className='product_price_all'><strong className='product_price'>Description: </strong>{product.desc}</p>
         <AddToCart product={product} />
       </div>
     </div>
