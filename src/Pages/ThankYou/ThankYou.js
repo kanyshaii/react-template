b860ac5399e77dc1a861776e85f22ed0a185d0d5
kanyshaii <kanyshaibaktybekova07@gmail.com>
2024-06-11 14:42:53 +0600

    added new styles

--- src/Pages/ThankYou/ThankYou.js
+++ src/Pages/ThankYou/ThankYou.js
@@ -1,7 +1,7 @@
 export default function ThankYou() {
   return (
     <div className="ThankYou">
-      <h1>Thank You!</h1>
+      <h1>THANK YOU!</h1>
       <p>Your order has been received.</p>
     </div>
   )
